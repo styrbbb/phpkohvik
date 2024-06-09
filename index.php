a06bf1a0b47a44e7de1b2c4992ba02c3b789e1ed
thorian <115464923+styrbbb@users.noreply.github.com>
2024-06-09 20:00:24 +0300

    Tieto KT

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" type="text/css" href="stylesk/styles.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
@@ -27,8 +26,17 @@
     </div>
     <?php include('config.php'); ?>
     <?php 
-    		$paring = 'SELECT * FROM kohvikud LIMIT 10';	
-            $valjund = $yhendus->query($paring);
+        $perPage = 10; // Kohvikute arv lehel
+        $currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $offset = ($currentPage - 1) * $perPage;
+
+        $totalQuery = 'SELECT COUNT(*) as total FROM kohvikud';
+        $totalResult = $yhendus->query($totalQuery);
+        $totalKohvikud = $totalResult->fetch_assoc()['total'];
+        $totalPages = ceil($totalKohvikud / $perPage);
+
+        $paring = "SELECT * FROM kohvikud LIMIT $perPage OFFSET $offset"; 
+        $valjund = $yhendus->query($paring);
         
     echo "<div class='container table-container'>";
         echo "<table class='table table-bordered text-start rounded-0 mt-1'>";
@@ -62,9 +70,13 @@
     ?>
     <div class="container">
         <div class="row">
-            <div class="col-12 d-flex justify-content-end">
-                <button class="btn btn-outline-secondary rounded-0 text-dark" id="prevPage"> < Eelmised </button><a>&nbsp</a>
-                <button class="btn btn-outline-secondary rounded-0 text-dark" id="nextPage"> Järgmised > </button>
+            <div class="col-12 d-flex justify-content-end mb-5">
+                <?php if($currentPage > 1): ?>
+                    <a href="?page=<?php echo $currentPage - 1; ?>" class="btn btn-outline-secondary rounded-0 text-dark"> < Eelmised </a>
+                <?php endif; ?>
+                <?php if($currentPage < $totalPages): ?>
+                    <a href="?page=<?php echo $currentPage + 1; ?>" class="btn btn-outline-secondary rounded-0 text-dark"> Järgmised > </a>
+                <?php endif; ?>
             </div>
         </div>
     </div>
@@ -72,7 +84,7 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-md-3 mx-auto">
+                <div class="col-md-6 mx-auto">
                     <h5 class="text-start">Thorian</h5>
                 </div>
             </div>
