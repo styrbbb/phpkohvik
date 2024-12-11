46e253a99aa2382cece621eba870e85b6c6c39b7
thorian <115464923+styrbbb@users.noreply.github.com>
2024-12-11 20:07:54 +0200

    php?

---
+++ h6/6_config.php
@@ -0,0 +1,16 @@
+<?php
+		//***protseduuriline***//
+		//sinu andmed
+	$db_server = 'localhost';
+	$db_andmebaas = 'muusikapood';
+	$db_kasutaja = 'Thorian';
+	$db_salasona = '8JyzR2EO6UOi';
+		//ühendus andmebaasiga
+	$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+	
+		//ühenduse kontroll
+	if(!$yhendus){
+		die('Ei saa ühendust andmebaasiga');
+	}
+
+?>
---
+++ h6/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="et">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP ja MySQL 6</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+
+	<?php include('6_config.php'); ?>
+	<?php
+
+	?>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+	</body>
+</html>
+
+
